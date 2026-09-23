95aa6cfc30ce58edac276cd8c2f6c42b0553e348
DgHaa <162941758+DgHaa@users.noreply.github.com>
2026-09-23 11:38:52 +0800

    Modify main.py by adding a character
    Added a character to the main.py file.

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ API 文档：
 import logging
 from contextlib import asynccontextmanager
 from pathlib import Path
-
+啊
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
